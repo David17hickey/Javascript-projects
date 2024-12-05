e12b3a2dfa1183fa42421099807a09664d5453d6
david hickey <162799896+David17hickey@users.noreply.github.com>
2024-12-05 19:58:14 +0000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Alert Example</title>
+    <script src="Js/main.js"></script>
+<body>
+    <button onclick="showAlert()">Click Me</button>
+</body>
+</html>
